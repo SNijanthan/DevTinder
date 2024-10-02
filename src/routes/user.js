ff3d1974d413ffbdbf5124547695b1ff0c205bb2
Nijanthan <nijanthan378@gmail.com>
2024-10-02 21:04:58 +0530

    gitignore updated

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequests = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
 const USER_SAFE_DATA = "firstName lastName about skills profilePic age gender";
 
-// Get all the PENDING connection requests for the loggedIn user
+// GET all the PENDING connection requests for the loggedIn user
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -25,7 +26,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
-// Get all the sent requests which is sent by loggedIn user
+// GET all the sent requests which is sent by loggedIn user
 
 userRouter.get("/user/requests/send", userAuth, async (req, res) => {
   try {
@@ -50,7 +51,7 @@ userRouter.get("/user/requests/send", userAuth, async (req, res) => {
   }
 });
 
-// Get all the "accepted" connections for the loggedIn user
+// GET all the "accepted" connections for the loggedIn user
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
@@ -79,6 +80,79 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   } catch (error) {
     res.status(400).send(`Error: ${error}`);
   }
+});
+
+// GET all the users from DB except some conditions
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all the profile except his own profile
+    // The existing connections should not show
+    //  User ignored people (user left swipes)
+    // Connection request already sent profile
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+
+    let limit = parseInt(req.query.limit) || 10;
+
+    limit = limit > 50 ? 50 : limit;
+
+    // const limit = Math.min(parseInt(req.query.limit) || 10, 50);
+
+    const skip = (page - 1) * limit;
+
+    // Finding all the connection request that user have sent and received
+    const connectionRequests = await ConnectionRequests.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    // using Set to find unique users
+
+    const hideUsersFromFeed = new Set();
+
+    // This used to find which users we should not show in feed
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    // DB condition to only new users profile should show
+    // Already request sent and request received profiles and own profile should not show
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send(`Error: ${error}`);
+  }
+
+  // Refactoring this code
+
+  // Use Set to store user IDs to hide
+
+  // const hideUsersFromFeed = new Set(
+  //   connectionRequests.flatMap(req => [req.fromUserId.toString(), req.toUserId.toString()])
+  // );
+
+  // Add logged-in user's ID to the Set
+
+  // hideUsersFromFeed.add(loggedInUser._id.toString());
+
+  // Fetch users excluding hidden ones
+
+  // const users = await User.find({
+  //   _id: { $nin: Array.from(hideUsersFromFeed) }
+  // })
 });
 
 module.exports = { userRouter };
